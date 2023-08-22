8ebc0b409415846c37e8de9f6a84d1e2a17a6ee8
leegeunhyeok <dev.ghlee@gmail.com>
2023-08-22 20:42:49 +0900

    chore: add result

--- src/block-scope-in-loop.js
+++ src/block-scope-in-loop.js
@@ -11,6 +11,19 @@ for (let i = 0; i < 10; i++) {
   });
 }
 
+/**
+ * # Hermes
+ * 
+ * - index 0: 10
+ * - index 5: 10
+ * - index 9: 10
+ * 
+ * # Node.js
+ *
+ * - index 0: 0
+ * - index 5: 5
+ * - index 9: 9
+ */
 print('index 0: ' + target[0]);
 print('index 5: ' + target[5]);
 print('index 9: ' + target[9]);
